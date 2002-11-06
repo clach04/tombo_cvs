1089b17c88a6823adf47dac8fa4bbd8c4bf54792
hirami <hirami>
2002-11-06 14:12:38 +0000

    Change path for CVS FIX: when source path include space, copy failed.

--- etc/mkbuild.pl
+++ etc/mkbuild.pl
@@ -1,16 +1,20 @@
 #
 
+use File::Glob ':glob';
+
 # TOMBO Build program for ActivePerl(Win32)
 
 ##########################################################################
 # Data definition
 ##########################################################################
 
-$version = "Tombo1_3";
-$buildroot = "R:\\work2\\TOMBO";
+$version = "Tombo1_4";
+$buildroot = "R:\\work2\\SF\\TOMBO";
+$docroot = "R:\\work2\\SF\\Docs";
 $builddir  = "R:\\temp\\Tombo-Rel";
 
 $be500root = "R:\\work2\\BE500SDK\\Installer\\Japanese PC Tools";
+$be300root = "R:\\work2\\BE300SDK_1012\\Installer\\English PC Tools";
 
 
 @buildlistCEJP = (
@@ -31,20 +35,19 @@ $be500root = "R:\\work2\\BE500SDK\\Installer\\Japanese PC Tools";
 	["Bin\\BE500", "$be500root\\Tombo.cbea"],
 	["Bin\\BE500", "$buildroot\\BE500\\Tombo\\MIPSRel\\Tombo.exe"],
 
-	["", "$buildroot\\Docs\\COPYING-2_0.txt"],
-	["", "$buildroot\\Docs\\Readme.txt"],
-	["Docs", "$buildroot\\Docs\\HTMLDoc\\*.html"],
-	["Docs\\image", "$buildroot\\Docs\\HTMLDoc\\image\\*.bmp"],
+	["", "$docroot\\Jp\\COPYING-2_0.txt"],
+	["", "$docroot\\Jp\\Readme.txt"],
+	["Docs", "$docroot\\Jp\\Docs\\*.html"],
+	["Docs\\image", "$docroot\\Jp\\Docs\\image\\*.bmp"],
 );
 
 @buildlistWin32JP = (
 	["", "$buildroot\\Win32\\Tombo\\Release\\Tombo.exe"],
 
-	["", "$buildroot\\Docs\\COPYING-2_0.txt"],
-	["", "$buildroot\\Docs\\Readme.txt"],
-	["Docs", "$buildroot\\Docs\\HTMLDoc\\*.html"],
-	["Docs\\image", "$buildroot\\Docs\\HTMLDoc\\image\\*.bmp"],
-
+	["", "$docroot\\Jp\\COPYING-2_0.txt"],
+	["", "$docroot\\Jp\\Readme.txt"],
+	["Docs", "$docroot\\Jp\\Docs\\*.html"],
+	["Docs\\image", "$docroot\\Jp\\Docs\\image\\*.bmp"],
 );
 
 @buildlistCEEN = (
@@ -60,15 +63,25 @@ $be500root = "R:\\work2\\BE500SDK\\Installer\\Japanese PC Tools";
 	["Bin\\PsPC\\SH3", "$buildroot\\PsPC\\CabWiz\\TomboE.mips.CAB"],
 	["Bin\\PsPC\\MIPS", "$buildroot\\PsPC\\CabWiz\\TomboE.sh3.CAB"],
 
-	["", "$buildroot\\Docs\\COPYING-2_0.txt"],
-	["", "$buildroot\\Docs\\Readme_E.txt"],
+	["Bin\\BE300", "$be300root\\Setup.exe"],
+	["Bin\\BE300", "$be300root\\Setup.ini"],
+	["Bin\\BE300", "$be300root\\Tombo.cbea"],
+	["Bin\\BE300", "$buildroot\\BE300\\Tombo\\MIPSRel\\Tombo.exe"],
+
+	["", "$docroot\\En\\COPYING-2_0.txt"],
+	["", "$docroot\\En\\Readme.txt"],
+	["Docs", "$docroot\\En\\Docs\\*.html"],
+#	["Docs\\image", "$docroot\\En\\Docs\\image\\*.bmp"],
+
 );
 
 @buildlistWin32EN = (
 	["", "$buildroot\\Win32\\Tombo\\ReleaseE\\Tombo.exe"],
 
-	["", "$buildroot\\Docs\\COPYING-2_0.txt"],
-	["", "$buildroot\\Docs\\Readme.txt"],
+	["", "$docroot\\En\\COPYING-2_0.txt"],
+	["", "$docroot\\En\\Readme.txt"],
+	["Docs", "$docroot\\En\\Docs\\*.html"],
+#	["Docs\\image", "$docroot\\En\\Docs\\image\\*.bmp"],
 );
 
 ##########################################################################
@@ -84,7 +97,6 @@ use File::DosGlob 'glob';
 &cpfile("$builddir\\JP-Win32\\$version", \@buildlistWin32JP);
 &cpfile("$builddir\\EN-Win32\\$version", \@buildlistWin32EN);
 
-
 sub cpfile {
 	my ($buildto, $list) = @_;
 	my ($i, $num);
@@ -98,7 +110,7 @@ sub cpfile {
 		mkpath("$buildto\\$dst") || die "mkdir $buildto\\$dst : $!" unless (-d "$buildto\\$dst");
 
 		# Check file existance and copy it.
-		foreach $f (glob($src)) {
+		foreach $f (bsd_glob($src)) {
 			die "$f not exist!" unless (-f "$f");
 			print "$f -> $buildto\\$dst\n";
 			copy("$f", "$buildto\\$dst\\") || die "$f : $!";
