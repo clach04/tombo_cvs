5f3912986036396b967edd808dac20f9b1bbe5f2
hirami <hirami>
2002-11-06 14:10:33 +0000

    Change path for CVS

--- PsPC/CabWiz/mkcab.bat
+++ PsPC/CabWiz/mkcab.bat
@@ -1,1 +1,1 @@
-"C:\Windows CE Tools\wce211\MS Palm Size PC\support\appinst\bin\cabwiz.exe" "R:\work2\Tombo\PsPC\CabWiz\Tombo.inf" /err cabwiz.err /cpu sh3 mips
+"C:\Windows CE Tools\wce211\MS Palm Size PC\support\appinst\bin\cabwiz.exe" "R:\work2\SF\Tombo\PsPC\CabWiz\Tombo.inf" /err cabwiz.err /cpu sh3 mips
