dc299a0cdf16587f51a787bd2fedcf38e97b1059
hirami <hirami>
2003-04-03 16:30:09 +0000

    B096(SNAPSHOT 20030404)

--- Src/AboutDialog.cpp
+++ Src/AboutDialog.cpp
@@ -12,7 +12,8 @@
 
 // version info
 
-#define TOMBO_VERSION_INFO TEXT("Tombo version 1.7 beta 1")
+//#define TOMBO_VERSION_INFO TEXT("Tombo version 1.7 beta 2-pre2")
+#define TOMBO_VERSION_INFO TEXT("Tombo 20030404")
 
 // コピーライト表記
 
